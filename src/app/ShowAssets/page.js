4eb70a36e25be00c657141e719a950f8cb7963b6
tderamus <deramust@gmail.com>
2024-11-24 22:24:51 -0600

    added general asset card, asset card api, and assets page for rendering assets to the DOM

---
+++ src/app/ShowAssets/page.js
@@ -0,0 +1,31 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import AssetCard from '../../components/AssetCard';
+import getAllAssets from '../../api/assetData';
+
+export default function ShowAssets() {
+  const [devices, setDevices] = useState([]);
+
+  const getDevices = () => {
+    getAllAssets()
+      .then((data) => {
+        setDevices(data);
+      })
+      .catch((error) => {
+        console.error('error fetching data', error);
+      });
+  };
+
+  useEffect(() => {
+    getDevices();
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+      {devices.map((assets) => (
+        <AssetCard key={assets.firebaseKey} assetObj={assets} onUpdate={getDevices} />
+      ))}
+    </div>
+  );
+}
